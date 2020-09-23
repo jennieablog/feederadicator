7b96779d97165f39eb839753f831df0315c17701
Jennie Ron Ablog <jennieablog@gmail.com>
2020-09-23 12:09:13 +0800

    Added eradicator selectors for twitter trending and feed

--- eradicate.js
+++ eradicate.js
@@ -1,2 +1,7 @@
+// Facebook Stories, News Feed, and Rooms
 document.querySelectorAll('[aria-label="Stories"]')[0].remove();
+document.querySelectorAll('[aria-label="VideoChatHomeUnit"]')[0].remove();
 document.querySelectorAll('[role="feed"]')[0].remove();
+// Twitter Trending Topics and Feed
+document.querySelectorAll('[aria-labelledby="accessible-list-0"]')[0].remove();
+document.querySelectorAll('[aria-labelledby="accessible-list-1"]')[0].remove();
